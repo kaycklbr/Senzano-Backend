97f64cbf568a67e40e144a8637a5be76a5bf4cf0
Kayck L Brito <kaycklbr@gmail.com>
2025-11-10 13:19:02 -0300

    cors

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,7 @@ return [
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['Access-Control-Allow-Origin', 'Authorization', 'Content-Type'],
+    'allowed_headers' => ['Authorization', 'Content-Type'],
 
     'exposed_headers' => [],
 
