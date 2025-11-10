52498db5109066256b7d14e724693e67fd89a29d
Kayck L Brito <kaycklbr@gmail.com>
2025-11-10 13:24:32 -0300

    cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Fruitcake\Cors\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
             // \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Specialtactics\L5Api\Http\Middleware\SnakeCaseInputParameterKeys::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            HandleCors::class,
         ]);
 
         // Aliases
