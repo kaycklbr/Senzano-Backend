57d4a8b18d9c107989f1039d99db45a5a205565c
Kayck L Brito <kaycklbr@gmail.com>
2025-11-21 10:48:12 -0300

    vrsync

--- app/Http/Controllers/VrSyncController.php
+++ app/Http/Controllers/VrSyncController.php
@@ -14,33 +14,33 @@ class VrSyncController extends Controller
         });
 
         $xml = new \SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><ListingDataFeed xmlns="http://www.vivareal.com/schemas/1.0/VRSync" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.vivareal.com/schemas/1.0/VRSync http://xml.vivareal.com/vrsync.xsd"></ListingDataFeed>');
-        
+
         // Header
         $header = $xml->addChild('Header');
         $header->addChild('Provider', 'Senzano Empreendimentos');
         $header->addChild('Email', 'contato@senzano.com.br');
         $header->addChild('ContactName', 'Senzano');
         $header->addChild('PublishDate', now()->toISOString());
-        $header->addChild('Telephone', '67-99841-0528');
+        $header->addChild('Telephone', '67-99877 7583');
 
         // Listings
         $listings = $xml->addChild('Listings');
 
         foreach ($properties as $property) {
             $listing = $listings->addChild('Listing');
-            
+
             $listing->addChild('ListingID', $property->external_id ?: $property->id);
             $listing->addChild('Title', htmlspecialchars($property->title));
-            
+
             // TransactionType baseado no crm_origin
             if ($property->crm_origin === 'imobzi') {
                 $listing->addChild('TransactionType', 'For Sale');
             } elseif ($property->crm_origin === 'imoview') {
                 $listing->addChild('TransactionType', 'For Rent');
             }
-            
+
             $listing->addChild('PublicationType', $property->destaque ? 'PREMIUM' : 'STANDARD');
-            $listing->addChild('DetailViewUrl', "https://senzanoempreendimentos.com.br/imovel/{$property->slug}");
+            $listing->addChild('DetailViewUrl', "https://dev.senzanoempreendimentos.com.br/imovel/{$property->slug}");
 
             // Media
             if ($property->cover_photo) {
@@ -60,7 +60,7 @@ class VrSyncController extends Controller
             $details = $listing->addChild('Details');
             $details->addChild('UsageType', $property->finality === 'commercial' ? 'Commercial' : 'Residential');
             $details->addChild('PropertyType', $property->finality === 'commercial' ? 'Commercial / Office' : 'Residential / ' . $property->property_type);
-            
+
             $description = $details->addChild('Description');
             $description[0] = strip_tags($property->description ?: $property->title);
 
@@ -99,13 +99,13 @@ class VrSyncController extends Controller
             // Location
             $location = $listing->addChild('Location');
             $location->addAttribute('displayAddress', 'All');
-            
+
             $country = $location->addChild('Country', 'Brasil');
             $country->addAttribute('abbreviation', 'BR');
-            
+
             $state = $location->addChild('State', $property->state ?: 'MS');
             $state->addAttribute('abbreviation', 'MS');
-            
+
             $location->addChild('City', $property->city ?: 'Campo Grande');
             if ($property->neighborhood) $location->addChild('Neighborhood', htmlspecialchars($property->neighborhood));
             if ($property->address) $location->addChild('Address', htmlspecialchars($property->address));
@@ -118,7 +118,7 @@ class VrSyncController extends Controller
             $contact = $listing->addChild('ContactInfo');
             $contact->addChild('Name', 'Senzano Empreendimentos');
             $contact->addChild('Email', 'contato@senzano.com.br');
-            $contact->addChild('Website', 'https://senzanoempreendimentos.com.br');
+            $contact->addChild('Website', 'https://dev.senzanoempreendimentos.com.br');
             $contact->addChild('Telephone', '(67) 99841-0528');
         }
 
