f564cd0a89e0d10cc5bf95e9a53cf69bf1622b64
Kayck L Brito <kaycklbr@gmail.com>
2025-11-21 11:09:26 -0300

    schedule

--- routes/console.php
+++ routes/console.php
@@ -7,3 +7,8 @@ use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    \Log::info('Executou via schedule!');
+})->everyMinute();
+// Schedule::command('properties:import')->everySixHours();
