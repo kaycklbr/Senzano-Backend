4f51a38b115bcc15f749e4f2401ce15805a8b6c2
Kayck L Brito <kaycklbr@gmail.com>
2025-11-21 11:13:25 -0300

    schedule

--- routes/console.php
+++ routes/console.php
@@ -8,7 +8,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::call(function () {
-    \Log::info('Executou via schedule!');
-})->everyMinute();
-// Schedule::command('properties:import')->everySixHours();
+// Schedule::call(function () {
+//     \Log::info('Executou via schedule!');
+// })->everyMinute();
+Schedule::command('properties:import')->everySixHours();
